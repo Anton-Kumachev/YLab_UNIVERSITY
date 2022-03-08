1f23935c1186ab4a674e4a0bb3a5463859371764
Anton-Kumachev <anton.kumachev@inbox.ru>
2022-03-09 00:22:07 +0700

    Refactoring code Main_X_O.java

--- src/lecture02/homework/dop02_X_O/Main_X_O.java
+++ src/lecture02/homework/dop02_X_O/Main_X_O.java
@@ -3,11 +3,6 @@ package lecture02.homework.dop02_X_O;
 //Не простая реализация простой игры "КРЕСТИКИ-НОЛИКИ"
 
 import java.io.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main_X_O {
 
@@ -19,6 +14,9 @@ public class Main_X_O {
     static int countPlayer01Win = 0;
     static int countPlayer02Win = 0;
     static int countNoWinner = 0;
+    static String fileRecord = "rating_records.txt";
+    static boolean turn = true;
+    static int countPlaceField = 0;
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -44,30 +42,23 @@ public class Main_X_O {
     void game() throws IOException {
 
         while (true) {
-            movePlayer01(); //Ход игрока 1(X)
+            movePlayer(); //Ход игрока 1(X)
             if (checkWinner(SIGN_PLAYER01)) {
                 System.out.println(namePlayer1 + " ВЫИГРАЛ!!! КРЕСТИКИ РУЛЯТ!");
                 countPlayer01Win++;
                 break;
             }
-            if (playFieldFull()) {
-                System.out.println("ВСЕ ЯЧЕЙКИ ЗАПОЛНЕНЫ. В ЭТОТ РАЗ У ВАС НИЧЬЯ!");
-                countNoWinner++;
-                break;
-            }
-            printPlayField(); //Вывод игрового поля после хода Игрока 1
-            movePlayer02(); //Ход игрока 2(O)
-            printPlayField(); //Вывод игрового поля после хода Игрока 2
             if (checkWinner(SIGN_PLAYER02)) {
                 System.out.println(namePlayer2 + " ВЫИГРАЛ!!! НОЛИКАМ СНОВА КАТИТ!");
                 countPlayer02Win++;
                 break;
             }
-            if (playFieldFull()) {
+            if (countPlaceField >= 9) {
                 System.out.println("ВСЕ ЯЧЕЙКИ ЗАПОЛНЕНЫ. В ЭТОТ РАЗ У ВАС НИЧЬЯ!");
                 countNoWinner++;
                 break;
             }
+            printPlayField(); //Вывод игрового поля после хода Игрока
         }
         System.out.println("КОНЕЦ ИГРЫ!");
         printPlayField();
@@ -76,16 +67,29 @@ public class Main_X_O {
         System.out.println();
         System.out.println("СЫГРАЕМ ЕЩЁ РАЗ??? (Ответьте: \"Да\" или \"Нет\")");
 
-        while (repeatGame()) { //Если repeatGame() == true то игра запуститься ещё раз, если false,
-            // то цикл прервётся и программа перестанет выполянться
+        if (repeatGame()) {
+            countPlaceField = 0;
             new Main_X_O().game();
-            break;
+        } else {
+            result();
+        }
+    }
+
+    static void result() { //Метод вывода рейтинга в консоль по окончанию игр
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileRecord))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line + "\n");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     boolean repeatGame() throws IOException { //Повтор игры
         String answer = reader.readLine();
-        if (answer.equals("Да") || answer.equals("да")) {
+        if (answer.toLowerCase().equals("да")) {
             return true;
         } else
             return false;
@@ -105,48 +109,45 @@ public class Main_X_O {
         System.out.println("Введите имя игрока 2(O):");
         namePlayer2 = reader.readLine();
     }
-    void movePlayer01() throws IOException {
+    void movePlayer() throws IOException {
         System.out.println();
         int x, y;
         do {
-            System.out.println("Игрок 1(X), чтобы начать ход, введите номер строки(по горизонтали) от 1 до 3:");
+            if(turn) {
+                System.out.println("Игрок 1(X), чтобы начать ход, введите номер строки(по горизонтали) от 1 до 3:");
+            } else {
+                System.out.println("Игрок 2(O), чтобы начать ход, введите номер строки(по горизонтали) от 1 до 3:");
+            }
             x = Integer.parseInt(reader.readLine()) - 1;
-            System.out.println("Игрок 1(X), чтобы завершить ход, введите номер столбца(по вертикали) от 1 до 3:");
-            y = Integer.parseInt(reader.readLine()) - 1;
 
-            if (playField[x][y] == SIGN_PLAYER02) {
-                System.out.println("Эта ячейка уже занята ноликами. Повторите попытку заново!");
+            if(turn) {
+                System.out.println("Игрок 1(X), чтобы завершить ход, введите номер столбца(по вертикали) от 1 до 3:");
+            } else {
+                System.out.println("Игрок 2(O), чтобы завершить ход, введите номер столбца(по вертикали) от 1 до 3:");
             }
-            if (playField[x][y] == SIGN_PLAYER01) {
-                System.out.println("Эта ячейка уже занята крестиками. Повторите попытку заново!");
-            }
+            y = Integer.parseInt(reader.readLine()) - 1;
         } while (!checking(x, y));
+        if (turn) {
             playField[x][y] = SIGN_PLAYER01;
-    }
-
-    void movePlayer02() throws IOException {
-        System.out.println();
-        int x, y;
-        do {
-            System.out.println("Игрок 2(O), чтобы начать ход, введите номер строки(по горизонтали) от 1 до 3:");
-            x = Integer.parseInt(reader.readLine()) - 1;
-            System.out.println("Игрок 2(O), чтобы завершить ход, введите номер столбца(по вертикали) от 1 до 3:");
-            y = Integer.parseInt(reader.readLine()) - 1;
-
-            if (playField[x][y] == SIGN_PLAYER01) {
-                System.out.println("Эта ячейка уже занята крестиками. Повторите попытку заново!");
-            }
-            if (playField[x][y] == SIGN_PLAYER02) {
-                System.out.println("Эта ячейка уже занята ноликами. Повторите попытку заново!");
-            }
-        } while (!checking(x, y)) ;
+            turn = !turn;
+        } else {
             playField[x][y] = SIGN_PLAYER02;
+            turn = !turn;
+        }
+        countPlaceField++;
     }
+
     boolean checking(int x, int y) { //Проверка введённых чисел при совершении хода
         if (x < 0 || y < 0 || x >= 3 || y >= 3) {
             System.out.println();
             System.out.println("Вы вышли за пределы допустимых значений, сосредоточтесь и повторите попытку.");
             System.out.println();
+            return false;
+        } else if (playField[x][y] == SIGN_PLAYER02) {
+            System.out.println("Эта ячейка уже занята ноликами. Повторите попытку заново!");
+            return false;
+        } else if (playField[x][y] == SIGN_PLAYER01) {
+            System.out.println("Эта ячейка уже занята крестиками. Повторите попытку заново!");
             return false;
         }
         return playField[x][y] == SIGN_EMPTY;
@@ -163,36 +164,27 @@ public class Main_X_O {
         }
     }
 
-    boolean playFieldFull() { //Метод проверка заполненности поля
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (playField[i][j] == SIGN_EMPTY)
-                    return false;
-            }
-        }
-        return true;
-    }
-
     boolean checkWinner(char sign) { //Метод проверки на победителя
         for (int i = 0; i < 3; i++) {
             if((playField[i][0] == sign && playField[i][1] == sign && playField[i][2] == sign) ||
                     (playField[0][i] == sign && playField[1][i] == sign && playField[2][i] == sign))
-               return true;
+                return true;
             if ((playField[0][0] == sign && playField[1][1] == sign && playField[2][2] == sign) ||
-               (playField[2][0] == sign && playField[1][1] == sign && playField[0][2] == sign))
-               return true;
+                    (playField[2][0] == sign && playField[1][1] == sign && playField[0][2] == sign))
+                return true;
         }
-    return false;
+        return false;
     }
 
     void recordPlayerRating() {
-        String fileRecord = "rating_records.txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileRecord))) {
+            writer.write("\n");
+            writer.write("РЕЙТИНГ ИГРОКОВ: \n");
             writer.write("Игрок 1: " + namePlayer1 + " | " + "Победы: " + countPlayer01Win + "\n");
             writer.write("Игрок 2: " + namePlayer2 + " | " + "Победы: " + countPlayer02Win + "\n");
             writer.write("Ничьи: " + countNoWinner + "\n");
         }   catch (IOException e) {
-                    e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
